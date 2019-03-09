46e64416c1b329e852d71d51009646c0bf081bc6
studentenclubthor <44543574+studentenclubthor@users.noreply.github.com>
2019-03-09 18:29:14 +0100

    Update application/config/config.php

--- application/config/config.php
+++ application/config/config.php
@@ -54,8 +54,10 @@ define('URL', URL_PROTOCOL . URL_DOMAIN . "/");
  */
 define('DB_TYPE', 'mysql');
 define('DB_HOST', '127.0.0.1');
+define('DB_PORT', '3306');
 define('DB_NAME', 'THOR');
 define('DB_USER', 'root');
+define('DB_PASS', 'RagnarokWasJustOk');
 define('DB_CHARSET', 'utf8');
 // define('DB_CHARSET', 'latin');
 
