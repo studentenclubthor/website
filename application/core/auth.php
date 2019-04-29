14098e5eac80a4d1e94fe7503faeab0c06d5c016
studentenclubthor <studentenclub.thor@gmail.com>
2019-04-29 22:14:46 +0200

    Update auth.php

--- application/core/auth.php
+++ application/core/auth.php
@@ -2,13 +2,12 @@
 
 class Auth extends Controller{
 	
-	public function __construct()
-	  {
+	public function __construct(){
 		//Calling your Model class for page
 		parent::__construct();
 		
 		
-		if(isset($_COOKIE['thorsessionid']) {
+		if(isset($_COOKIE['thorsessionid'])){
 			$ifSessionExists = $this->model->checksession($_COOKIE['thorsessionid']);
 			if(! $ifSessionExists) {
 				setcookie("thorsessionid", '', time() - 10, "/");
@@ -20,7 +19,6 @@ class Auth extends Controller{
 			echo "else";
 			//header("Location: ". URL . "login");
 		}
-	  }
-	  
+	} 
 }
 ?>
